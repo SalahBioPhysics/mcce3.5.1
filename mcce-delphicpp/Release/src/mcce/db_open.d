7baf1470ac0f8ecc110ccf8b36d4a78314093507
SALAH SALAH <salah@master.cl.cuny.edu>
2017-10-13 18:16:43 -0400

    added two flags for old/new delphi

--- mcce-delphicpp/Release/src/mcce/db_open.d
+++
@@ -1,6 +0,0 @@
-src/mcce/db_open.d: ../src/mcce/db_open.cpp ../src/mcce/gdbm.h \
- ../src/mcce/mcce.h
-
-../src/mcce/gdbm.h:
-
-../src/mcce/mcce.h:
--- mcce-delphicpp/Release/src/mcce/surfw_l2.d
+++
@@ -1,3 +0,0 @@
-src/mcce/surfw_l2.d: ../src/mcce/surfw_l2.cpp ../src/mcce/mcce.h
-
-../src/mcce/mcce.h:
